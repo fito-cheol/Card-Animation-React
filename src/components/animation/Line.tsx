9bf3e9d1b17e25f2e14c97eb3f0ae79e7489936b
LAPTOP-TQFM3LTV\dmsgh <dmsghs92@gmail.com>
2024-05-06 23:00:04 +0900

    feat: global.scss 추가

--- src/components/animation/Line.tsx
+++ src/components/animation/Line.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import './Line.scss';
-import useMouse from '../../hooks/useMouse';
+import useMouse from '@/hooks/useMouse';
 
-export default function Line({ children, rotationProps, positionProps }: any) {
-  const { rotation, position, handleMouseMove, handleMouseLeave } = useMouse();
-  let rotationValue = rotationProps || rotation;
-  let positionValue = positionProps || position;
+export default function Line({ children, dynamicStylesProps }: any) {
+  const { handleMouseMove, handleMouseLeave, dynamicStyles } = useMouse();
+  let dynamicStylesValue = dynamicStylesProps || dynamicStyles;
+
   return (
-    <div className='line__wrapper' onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
-      <div className='line' style={{ backgroundPosition: (rotationValue.x + rotationValue.y) * 2 }}></div>
+    <div
+      className='line__wrapper'
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={dynamicStylesValue as React.CSSProperties}
+    >
+      <div className='line' />
       {children}
     </div>
   );
